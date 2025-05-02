f42cc93e20b296e7779f886a5fd084fb3c9cd9e8
GiovaniFZ <giovanifinazzi@gmail.com>
2025-05-02 13:12:13 -0300

    imp: Create MMS calculator (not really calculating for now)

--- queue_model_factory.py
+++ queue_model_factory.py
@@ -1,4 +1,5 @@
 from calculators.mm1 import MM1Calculator
+from calculators.mms import MMSCalculator
 from measures import MeasuresCalculator
 from queue_input_data import QueueInputData
 
@@ -7,5 +8,6 @@ class QueueMeasureCalculatorFactory:
     def create(self, data: QueueInputData) -> MeasuresCalculator:
         if data.A == "M" and data.B == "M" and data.m == 1:
             return MM1Calculator(data)
-        else:
-            raise NotImplementedError("Modelo de fila ainda não implementado.")
+        elif data.A == "M" and data.B == "M" and data.m > 1:
+            return MMSCalculator(data)
+        raise NotImplementedError("Modelo de fila ainda não implementado.")
