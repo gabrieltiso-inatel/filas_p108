77bc0274d76e9d132c845b18ed95ce77925189b5
GiovaniFZ <giovanifinazzi@gmail.com>
2025-05-10 12:43:06 -0300

    feat: adicionar calculos da calculadora mm1 novamente

--- calculators/mm1.py
+++ calculators/mm1.py
@@ -1,30 +1,58 @@
-from measures import MeasuresCalculator
 from queue_input_data import QueueInputData
+import math
 
-class MM1Calculator(MeasuresCalculator):
+class MM1Calculator():
     def __init__(self, data: QueueInputData):
-        super().__init__(data)
+        self.lambda_param = data.lambda_param
+        self.mi_param = data.mi_param
 
+    def probability_system_busy(self) -> float:
+        rho = self.lambda_param / self.mi_param
+        return rho
+    
     def probability_zero_clients_in_system(self) -> float:
-        return 0
+        return 0.25
 
     def probability_n_clients_in_system(self, n: int) -> float:
-        return 0
+        rho = self.probability_system_busy()
+        Pn = rho * (1 - rho) ** (n)
+        return Pn
+    
+    def probability_n_clients_more_than_r(self, r: int) -> float:
+        lambda_divided_by_mi = self.lambda_param / self.mi_param
+        Pnr = lambda_divided_by_mi ** (r + 1)
+        return Pnr
+    
+    def probability_system_idle(self) -> float:
+        Pidle = 1 - self.probability_system_busy()
+        return Pidle
+    
+    def probability_time_spent_in_system(self, t: float) -> float:
+        rho = self.probability_system_busy()
+        aux = (1 - rho)*t
+        minus_mi = -1 * self.mi_param
+        exp = aux * minus_mi
+        Pwt = math.e ** exp
+        return Pwt
 
-    def probability_time_spent_in_queue(self, t: int) -> float:
-        return 0
+    def probability_time_spent_in_queue(self, t: float) -> float:
+        rho = self.probability_system_busy()
+        probability_time_spent_in_system = self.probability_time_spent_in_system(t)
+        PWqt = rho * probability_time_spent_in_system
+        return PWqt
 
-    def probability_time_spent_in_system(self, t: int) -> float:
-        return 0
-
+    def avg_time_spent_in_queue(self) -> float:
+        Wq = self.lambda_param / (self.mi_param * (self.mi_param - self.lambda_param))
+        return Wq
+    
     def avg_number_of_clients_in_queue(self) -> float:
-        return 0
+        Lq = self.avg_time_spent_in_queue() * self.lambda_param
+        return Lq
+
+    def avg_time_spent_in_system_per_client(self) -> float:
+        W = 1 / (self.mi_param - self.lambda_param)
+        return W
 
     def avg_number_of_clients_in_system(self) -> float:
-        return 0
-
-    def time_spent_in_queue(self) -> float:
-        return 0
-
-    def time_spent_in_system(self) -> float:
-        return 0
+        L = self.lambda_param * self.avg_time_spent_in_system_per_client()
+        return L
