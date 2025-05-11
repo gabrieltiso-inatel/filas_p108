ccc7f9074d641dbc1507135044ba1133775a6e1c
GiovaniFZ <giovanifinazzi@gmail.com>
2025-05-11 18:02:22 -0300

    ref: change if else to match case

--- queues/mms/menu.py
+++ queues/mms/menu.py
@@ -28,43 +28,44 @@ def display_menu():
 def handle_mm1_queue(queue: MM1Queue):
     while True:
         display_menu()
-        choice = input("Escolha uma opção: ")
+        choice = int(input("Escolha uma opção: "))
 
-        if choice == "1":
-            n = int(input("Digite o valor de n: "))
-            result = queue.prob_n_clients_in_system(n)
-            print(f"Probabilidade de {n} clientes no sistema (Pn): {result}")
-        elif choice == "2":
-            result = queue.prob_system_empty()
-            print(f"Probabilidade de o sistema estar vazio (P0): {result}")
-        elif choice == "3":
-            result = queue.prob_system_busy()
-            print(f"Probabilidade de o sistema estar ocupado (1 - P0): {result}")
-        elif choice == "4":
-            result = queue.avg_number_clients_in_system()
-            print(f"Número médio de clientes no sistema (L): {result}")
-        elif choice == "5":
-            result = queue.avg_number_clients_in_queue()
-            print(f"Número médio de clientes na fila (Lq): {result}")
-        elif choice == "6":
-            result = queue.avg_time_in_system()
-            print(f"Tempo médio no sistema (W): {result}")
-        elif choice == "7":
-            result = queue.avg_time_in_queue()
-            print(f"Tempo médio na fila (Wq): {result}")
-        elif choice == "8":
-            t = float(input("Digite o valor de t: "))
-            result = queue.prob_wait_in_system_bigger_than(t)
-            print(f"Probabilidade de tempo de espera no sistema maior que {t} (P(W > t)): {result}")
-        elif choice == "9":
-            t = float(input("Digite o valor de t: "))
-            result = queue.prob_wait_in_queue_bigger_than(t)
-            print(f"Probabilidade de tempo de espera na fila maior que {t} (P(Wq > t)): {result}")
-        elif choice == "10":
-            print("Saindo do menu MM1...")
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+        match choice:
+            case 1:
+                n = int(input("Digite o valor de n: "))
+                result = queue.prob_n_clients_in_system(n)
+                print(f"Probabilidade de {n} clientes no sistema (Pn): {result}")
+            case 2:
+                result = queue.prob_system_empty()
+                print(f"Probabilidade de o sistema estar vazio (P0): {result}")
+            case 3:
+                result = queue.prob_system_busy()
+                print(f"Probabilidade de o sistema estar ocupado (1 - P0): {result}")
+            case 4:
+                result = queue.avg_number_clients_in_system()
+                print(f"Número médio de clientes no sistema (L): {result}")
+            case 5:
+                result = queue.avg_number_clients_in_queue()
+                print(f"Número médio de clientes na fila (Lq): {result}")
+            case 6:
+                result = queue.avg_time_in_system()
+                print(f"Tempo médio no sistema (W): {result}")
+            case 7:
+                result = queue.avg_time_in_queue()
+                print(f"Tempo médio na fila (Wq): {result}")
+            case 8:
+                t = float(input("Digite o valor de t: "))
+                result = queue.prob_wait_in_system_bigger_than(t)
+                print(f"Probabilidade de tempo de espera no sistema maior que {t} (P(W > t)): {result}")
+            case 9:
+                t = float(input("Digite o valor de t: "))
+                result = queue.prob_wait_in_queue_bigger_than(t)
+                print(f"Probabilidade de tempo de espera na fila maior que {t} (P(Wq > t)): {result}")
+            case 10:
+                print("Saindo do menu MM1...")
+                break
+            case _:
+                print("Opção inválida. Tente novamente.")
 
         input("Pressione Enter para continuar...")
         clear_screen()
@@ -73,42 +74,43 @@ def handle_mm1_queue(queue: MM1Queue):
 def handle_mms_queue(queue: MMsQueue):
     while True:
         display_menu()
-        choice = input("Escolha uma opção: ")
+        choice = int(input("Escolha uma opção: "))
 
-        if choice == "1":
-            n = int(input("Digite o valor de n: "))
-            result = queue.prob_n_clients_in_system(n)
-            print(f"Probabilidade de {n} clientes no sistema (Pn): {result}")
-        elif choice == "2":
-            result = queue.prob_zero_clients_in_system()
-            print(f"Probabilidade de o sistema estar vazio (P0): {result}")
-        elif choice == "3":
-            print("Essa funcionalidade não está implementada para MMs.")
-        elif choice == "4":
-            result = queue.avg_number_clients_in_system()
-            print(f"Número médio de clientes no sistema (L): {result}")
-        elif choice == "5":
-            result = queue.avg_number_clients_in_queue()
-            print(f"Número médio de clientes na fila (Lq): {result}")
-        elif choice == "6":
-            result = queue.avg_time_in_system()
-            print(f"Tempo médio no sistema (W): {result}")
-        elif choice == "7":
-            result = queue.avg_time_in_queue()
-            print(f"Tempo médio na fila (Wq): {result}")
-        elif choice == "8":
-            t = float(input("Digite o valor de t: "))
-            result = queue.prob_time_spent_in_system_bigger_than(t)
-            print(f"Probabilidade de tempo de espera no sistema maior que {t} (P(W > t)): {result}")
-        elif choice == "9":
-            t = float(input("Digite o valor de t: "))
-            result = queue.prob_time_spent_in_queue_bigger_than(t)
-            print(f"Probabilidade de tempo de espera na fila maior que {t} (P(Wq > t)): {result}")
-        elif choice == "10":
-            print("Saindo do menu MMs...")
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+        match choice:
+            case 1:
+                n = int(input("Digite o valor de n: "))
+                result = queue.prob_n_clients_in_system(n)
+                print(f"Probabilidade de {n} clientes no sistema (Pn): {result}")
+            case 2:
+                result = queue.prob_zero_clients_in_system()
+                print(f"Probabilidade de o sistema estar vazio (P0): {result}")
+            case 3:
+                print("Essa funcionalidade não está implementada para MMs.")
+            case 4:
+                result = queue.avg_number_clients_in_system()
+                print(f"Número médio de clientes no sistema (L): {result}")
+            case 5:
+                result = queue.avg_number_clients_in_queue()
+                print(f"Número médio de clientes na fila (Lq): {result}")
+            case 6:
+                result = queue.avg_time_in_system()
+                print(f"Tempo médio no sistema (W): {result}")
+            case 7:
+                result = queue.avg_time_in_queue()
+                print(f"Tempo médio na fila (Wq): {result}")
+            case 8:
+                t = float(input("Digite o valor de t: "))
+                result = queue.prob_time_spent_in_system_bigger_than(t)
+                print(f"Probabilidade de tempo de espera no sistema maior que {t} (P(W > t)): {result}")
+            case 9:
+                t = float(input("Digite o valor de t: "))
+                result = queue.prob_time_spent_in_queue_bigger_than(t)
+                print(f"Probabilidade de tempo de espera na fila maior que {t} (P(Wq > t)): {result}")
+            case 10:
+                print("Saindo do menu MMs...")
+                break
+            case _:
+                print("Opção inválida. Tente novamente.")
 
         input("Pressione Enter para continuar...")
         clear_screen()
