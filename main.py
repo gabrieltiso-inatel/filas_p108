4337315dfe866486d9b0323e51d42b5cb8d9b452
GiovaniFZ <giovanifinazzi@gmail.com>
2025-05-17 16:42:18 -0300

    feat: implement mm1n queue

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from queues.mmsn import menu_mm1n
+
 def main():
-    pass
+    print("Bem-vindo ao simulador!")
+    print("1. M/M/s/N")
+    print("7. Sair")
+    choice = int(input("Digite o número da opção desejada: "))
+    match choice:
+        case 1:
+            menu_mm1n.run()
+        case 7:
+            exit()
+        case _:
+            print("Opção inválida. Tente novamente.")
+            main()
 
 if __name__ == "__main__":
     main()
--- queues/mmsn/queue.py
+++ queues/mmsn/queue.py
@@ -3,6 +3,9 @@ from queues.mmsn.input import Params
 
 class MMNQueue:
     def __init__(self, p: Params):
+        self.n = p.n
+        self.lmbd = p.lmbd
+        self.mu = p.mu
         self.p = p
 
 # Giovani
@@ -10,6 +13,49 @@ class MM1NQueue(MMNQueue):
     def __init__(self, p: Params):
         super().__init__(p)
         self.rho = (p.n * p.lmbd) / p.mu
+        
+    def prob_zero_clients_in_system(self): # P0
+        sum = 0
+        for i in range(self.n):
+            term_one = factorial(self.n)/factorial(self.n - i)
+            term_two = (self.lmbd / self.mu)**i
+            sum += term_one * term_two
+        return 1 / sum
+    
+    def prob_n_clients_in_system(self, n: int): # Pn
+        p0 = self.prob_zero_clients_in_system()
+        term_one = factorial(self.n)/(factorial(self.n - n))
+        term_two = (self.lmbd / self.mu)**n
+        return term_one * term_two * p0
+    
+    def avg_number_clients_in_queue(self):
+        term_one = (self.lmbd + self.mu)/(self.lmbd)
+        term_two = (1 - self.prob_zero_clients_in_system())
+        lq = self.n - (term_one*term_two)
+        return lq
+    
+    def avg_number_clients_in_system(self):
+        term_one = (self.mu/self.lmbd)
+        term_two = (1 - self.prob_zero_clients_in_system())
+        term_three = (term_one * term_two)
+        l = self.n - term_three
+        return l
+    
+    def avg_lambda(self):
+        return self.lmbd * (self.n - self.avg_number_clients_in_system())
+    
+    def avg_waiting_time_in_queue(self):
+        lq = self.avg_number_clients_in_queue()
+        avg_lambda = self.avg_lambda()
+        wq = lq / avg_lambda
+        return wq
+    
+    def avg_time_in_system(self):
+        l = self.avg_number_clients_in_system()
+        avg_lambda = self.avg_lambda()
+        W = l / avg_lambda 
+        return W
+            
 
 # Gabriel
 class MMsNQueue(MMNQueue):
