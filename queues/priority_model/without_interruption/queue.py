a6306aff8fad25c33d4df43dd297b28205b494d2
Lucas Fajardo <84720413+SilvergamesBR@users.noreply.github.com>
2025-06-14 21:19:48 -0300

    Fix(priorityQueue)Ç Fixes calculation to account for priority levels.

--- queues/priority_model/without_interruption/queue.py
+++ queues/priority_model/without_interruption/queue.py
@@ -1,49 +1,53 @@
 from math import factorial
 from queues.priority_model.with_interruption.input import Params
 
+
 class PriorityModelWithoutInterruption:
+    """
+    Calculates average waiting times and client counts for a non-preemptive priority queue without interruptions.
+    """
     def __init__(self, p: Params):
         self.p = p
 
-    def avg_waiting_time_in_system(self):
-        r = self.p.total_lambda / self.p.mu
-        sum_one = sum((r**j) / factorial(j) for j in range(self.p.s))
-        term_A = factorial(self.p.s) * ((self.p.s * self.p.mu - self.p.total_lambda) / (r**self.p.s)) * sum_one + self.p.s * self.p.mu
-        sum_k = 0
-        sum_k_1 = 0
-        for i in range(self.p.n):
-            sum_k = sum(self.p.lmbds[:i]) / (self.p.s * self.p.mu)
-            sum_k_1 = sum(self.p.lmbds[:i+1]) / (self.p.s * self.p.mu)
-        term_B = 1 - (sum_k_1 / (self.p.s * self.p.mu))
-        term_C = 1 - (sum_k / (self.p.s * self.p.mu))
-        den = (term_A * term_B * term_C) + (1 / self.p.mu)
-        result = 1 / den
-        return result
-    
-    def avg_waiting_time_in_queue(self):
-        avg_wait_in_system_arr = self.avg_waiting_time_in_system()
-        avg_wait_in_queue_arr = []
+    def avg_waiting_time_in_system(self) -> list[float]:
+        """
+        Returns a list of average times each priority class spends in the system (waiting + service).
+        """
+        p = self.p
+        r = p.total_lambda / p.mu
+        sum_one = sum(r**j / factorial(j) for j in range(p.s))
+        term_A = (factorial(p.s) * (p.s * p.mu - p.total_lambda) / (r**p.s) * sum_one) + (p.s * p.mu)
 
-        for i in range(self.p.n):
-            result = avg_wait_in_system_arr[i] - (1 / self.p.mu)
-            avg_wait_in_queue_arr.append(result)
+        waiting_times = []
+        for i in range(p.n):
+            raw_sum_k = sum(p.lmbds[:i])
+            raw_sum_k1 = sum(p.lmbds[:i+1])
 
-        return avg_wait_in_queue_arr
-    
-    def avg_clients_in_system(self):
-        avg_wait_in_system_arr = self.avg_waiting_time_in_system()
-        avg_clients_in_system_arr = []
-        for i in range(self.p.n):
-            result = sum(self.p.lmbds[:i+1]) * avg_wait_in_system_arr[i]
-            avg_clients_in_system_arr.append(result)
+            term_B = 1 - (raw_sum_k1 / (p.s * p.mu))
+            term_C = 1 - (raw_sum_k / (p.s * p.mu))
 
-        return avg_clients_in_system_arr
-    
-    def avg_clients_in_queue(self):
-        avg_clients_in_system_arr = self.avg_clients_in_system()
-        avg_clients_in_queue_arr = []
-        for i in range(self.p.n):
-            result = avg_clients_in_system_arr[i] - (sum(self.p.lmbds[:i+1]) / self.p.mu)
-            avg_clients_in_queue_arr.append(result)
+            denom = (term_A * term_B * term_C) + (1 / p.mu)
+            waiting_times.append(1 / denom)
 
-        return avg_clients_in_queue_arr
+        return waiting_times
+
+    def avg_waiting_time_in_queue(self) -> list[float]:
+        """
+        Returns a list of average times each priority class spends waiting (excluding service).
+        """
+        W = self.avg_waiting_time_in_system()
+        return [w - (1 / self.p.mu) for w in W]
+
+    def avg_clients_in_system(self) -> list[float]:
+        """
+        Returns a list of average number of clients in system for each priority class.
+        """
+        W = self.avg_waiting_time_in_system()
+        return [sum(self.p.lmbds[:i+1]) * W[i] for i in range(self.p.n)]
+
+    def avg_clients_in_queue(self) -> list[float]:
+        """
+        Returns a list of average number of clients waiting in queue for each priority class.
+        """
+        L = self.avg_clients_in_system()
+        return [L[i] - (sum(self.p.lmbds[:i+1]) / self.p.mu) for i in range(self.p.n)]
